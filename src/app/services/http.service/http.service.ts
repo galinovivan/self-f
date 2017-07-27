e5dcc7a4b97f4845a7ee6426c7124105ec9914fb
galinovivan <9225913@gmail.com>
2017-07-27 07:03:58 +0300

    http

--- src/app/services/http.service/http.service.ts
+++ src/app/services/http.service/http.service.ts
@@ -13,17 +13,19 @@ export abstract class HttpService {
    getApiQuery(apiUrl : string) : Observable<any> {
         return this.http.get(apiUrl)
         .map((resp: Response) => {
-            let dataList = resp.json().data;
-            if (!dataList.isArray()) {
-                return dataList;
-            }
-            let dataObjects : Object[] = [];
+            let dataList = resp.json();
+            console.log(resp.json());
+            if (dataList instanceof Array) {
+               let dataObjects : Object[] = [];
 
             for (let index in dataList) {
-                let dataObject : Object = dataObjects[index];
-                Object.assign(dataObject, ...dataObjects);
+                let dataObject : Object = dataList[index];
+                dataObjects.push(dataObject);
+                console.log(dataObjects);
             }
             return dataObjects;
+        }
+        return dataList;
         })
    }
 }
